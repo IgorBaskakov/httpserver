4ce405941b1ef820a7d879a4edee9106e4ccfdb5
Игорь Баскаков <baskakovin@quadcom.ru>
2020-04-20 14:07:39 +0300

    add heroku

--- main.go
+++ main.go
@@ -1,11 +1,46 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi"
+)
+
+const port = 8090
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("start default router")
+
+		fmt.Printf("request: %#v\n", r)
+		fmt.Println("url:", r.URL.String())
+
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "done")
 	})
-	println("Start server at port 8080...")
-	http.ListenAndServe(":8080", nil)
+
+	srv := &http.Server{Addr: ":" + strconv.Itoa(port), Handler: r}
+	go func() {
+		fmt.Printf("start http server at port %d...\n", port)
+		if err := srv.ListenAndServe(); err != nil {
+			log.Printf("server error listen: %s", err)
+		}
+	}()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	srv.Shutdown(ctx)
+	cancel()
 }
