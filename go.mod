766ee25fff98d32f35d75d4c7f62979d708dbcca
Игорь Баскаков <baskakovin@quadcom.ru>
2020-04-20 18:28:10 +0300

    add request to gitlab

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/IgorBaskakov/httpserver
 
 go 1.14
 
-require github.com/go-chi/chi v4.1.1+incompatible
+require (
+	github.com/go-chi/chi v4.1.1+incompatible
+	github.com/rakyll/hey v0.1.3 // indirect
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"time"
 
 	"github.com/go-chi/chi"
@@ -43,21 +47,111 @@ func main() {
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	log.Print("start default router")
-
-	log.Printf("get request: %#v\n", r)
 	log.Print("method:", r.Method)
 
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintln(w, "New changes coming to server!")
 }
 
+type hookBody struct {
+	EventType string `json:"event_type"`
+	Project   struct {
+		ID uint `json:"id"`
+	} `json:"project"`
+	ObjAttributes struct {
+		IID          uint   `json:"iid"`
+		TargetBranch string `json:"target_branch"`
+		State        string `json:"state"`
+	} `json:"object_attributes"`
+}
+
 func hookHandler(w http.ResponseWriter, r *http.Request) {
 	log.Print("start hook router")
-
-	log.Printf("post request: %#v\n", r)
 	log.Print("method:", r.Method)
 
-	w.WriteHeader(http.StatusOK)
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("read request body error: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	hbody := hookBody{}
+	if err = json.Unmarshal(body, &hbody); err != nil {
+		log.Printf("read request body error: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	fmt.Printf("hook body: %+v\n", hbody)
+	fmt.Println(strings.Repeat("-", 100))
+
+	var commits []commit
+	if hbody.EventType == "merge_request" && hbody.ObjAttributes.State == "merged" {
+		commits, err = getCommits(hbody.Project.ID, hbody.ObjAttributes.IID)
+		if err != nil {
+			log.Printf("get commits error: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Printf("commits: %#v\n", commits)
+	}
+
 	w.Header().Add("Content-Type", "application/json")
-	w.Write([]byte(`{"result":"hook handler done"}`))
+
+	w.WriteHeader(http.StatusOK)
+	res, err := json.Marshal(commits)
+	if err != nil {
+		log.Printf("marshal result data error: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(res)
+}
+
+type commit struct {
+	ID         string `json:"id"`
+	Title      string `json:"title"`
+	AuthorName string `json:"author_name"`
+}
+
+func getCommits(id, iid uint) ([]commit, error) {
+	url := fmt.Sprintf("http://gitlab.alx/api/v4/projects/%d/merge_requests/%d/commits", id, iid)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	token := os.Getenv("TOKEN")
+	if token == "" {
+		return nil, errors.New("$TOKEN must be set")
+	}
+
+	req.Header.Add("Private-Token", token)
+
+	tr := &http.Transport{
+		MaxIdleConns:    10,
+		IdleConnTimeout: 30 * time.Second,
+	}
+	client := &http.Client{Transport: tr}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	commits := []commit{}
+	json.Unmarshal(body, &commits)
+	if err != nil {
+		return nil, err
+	}
+
+	return commits, nil
 }
